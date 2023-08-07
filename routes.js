e7083b6e0858e25e5849115486f7ef7f986407db
suraj pro <71000909+suraj719@users.noreply.github.com>
2023-08-07 20:51:34 +0530

    amzn rest

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const {getAmznProduct,getFktProduct} = require("./controllers/controller")
 const router = express.Router()
-router.route('/amzn').post(getAmznProduct)
+router.route('/amzn').get(getAmznProduct)
 router.route('/fkt').get(getFktProduct)
 
 
